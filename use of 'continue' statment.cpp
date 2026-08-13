d5fa34652a08d773b54e55a96e91bdc798303152
Krushna Barde <kgbarde@gmail.com>
2026-08-13 17:25:57 +0530

    How to use 'continue ' in our code

---
+++ use of 'continue' statment.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+int n ;
+for(int i=1; i<=20; i++){
+if (i==3 || i==8 ) continue;
+cout <<i<<endl;
+  }
+
+}
+/*we can use saperetly too,like 
+ if (i==3) continue;
+ if (i==8) continue; */
